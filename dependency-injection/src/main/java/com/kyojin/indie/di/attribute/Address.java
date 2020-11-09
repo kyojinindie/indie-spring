3777c3635646c957ec8cfcbe2ae1bac90964b794
kyojinindie <kyojinindie@gmail.com>
2020-11-08 20:11:01 -0600

    Update package attributes

--- dependency-injection/src/main/java/com/kyojin/indie/di/attribute/Address.java
+++ dependency-injection/src/main/java/com/kyojin/indie/di/attribute/Address.java
@@ -1,4 +1,4 @@
-package com.kyojin.indie.di;
+package com.kyojin.indie.di.attribute;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
--- dependency-injection/src/main/java/com/kyojin/indie/di/attribute/Person.java
+++ dependency-injection/src/main/java/com/kyojin/indie/di/attribute/Person.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.kyojin.indie.di.Address;
-
 /*
  * Powered by: Kyojin-Indie
  * 
